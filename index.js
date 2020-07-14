f6395074496562a75184c31fc7d96d2e94a97db7
Jessica Salbert <jessicarsalbert@gmail.com>
2020-07-14 16:18:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    var string = `${musicians[i]} plays ${instruments[i]}`
+    array.push(string);
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  while (i < facts.length) {
+    facts[i] += "!!!";
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++
+  } while (number < 15)
+  return array;
+}
